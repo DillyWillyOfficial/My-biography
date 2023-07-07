11958c5987679e3dc57757576f82366e6b4574f1
TXT-92 <txt.hackathon@TXTs-MBP-2.lan>
2023-07-06 23:10:20 -0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View 
+      style={styles.container}>
+      <Image 
+      style={{ width: 200, height: 200}}
+      source={{ 
+      uri: 'https://wallpapercave.com/wp/wp4867505.jpg'
+      }}
+      />
+      <Text style={styles.title}> Dillon Crawford</Text>
+      <Text style={styles.paragraph}>I am a 15 year old kid who has found a passion for code, in my freetime I would play video games or look up tutorials on how to code, I currently go to Torrence high and my goals are to go to a out of state college and work for as gaming companing such as riot games </Text>
       <StatusBar style="auto" />
-    </View>
+    </View> 
   );
 }
 
@@ -17,4 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 25,
+  }
 });
